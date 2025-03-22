709f2e9fc4777d9e3b6f5e0876242237c429c80a
haath <haath@protonmail.com>
2025-03-22 10:53:26 +0100

    Add build workflow

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # airflair
+
+
+
+# Dates
+
+The following date formats are supported:
+
+* YYYY-MM-DD
+* DD.MM (current year)
+* YYYYMMDD
+* DD.MM.YYYY
+* MM-DD (current year)
+* DD/MM/YYYY
+* DD/MM (current year)
+
+
+# Examples
+
+One-way trips from Amsterdam to Athens in the first two weeks of March
+
+```
+$ airflair AMS ATH --depart-from 01.03 --depart-to 15.03
+```
+
+Round-trips from Amsterdam to Athens on specific dates
+
+```
+$ airflair AMS ATH --depart 01.03 --return 12.03
+```
+
+Round-trips with a specific departure date, but a variety of return dates
+
+```
+$ airflair AMS ATH --depart 01.03 --return-from 05.03 --return-to 08.03
+```
+
+Round-trips spanning a specified number of days
+
+```
+$ airflair AMS ATH --depart-from 01.03 --depart-to 03.03 --days 5
+```
+
+```
+$ airflair AMS ATH --depart 01.03 --days-from 5 --days-to 7
+```
+
+Multi-leg trip: travel from Amsterdam to Athens on the first weekend in March, stay for 5 days, then travel to Warsaw, and on the 12th return to Amsterdam
+
+```
+$ airflair AMS --depart-from 01.03 --depart-to 03.03 \
+              ATH --stay 5 \
+              WAW --until 12.03 \
+              AMS
+```
--- src/cli/FlightsCli.hx
+++ src/cli/FlightsCli.hx
@@ -277,7 +277,7 @@ class FlightsCli
 
         if (help)
         {
-            Sys.println('Usage: flights-cli [from IATA] [to IATA]');
+            Sys.println('Usage: airflair [from IATA] [to IATA]');
             Sys.println(argHandler.getDoc());
 
             if (helpExtended)
@@ -697,24 +697,24 @@ The following date formats are supported:
 
 One-way trips from Amsterdam to Athens in the first two weeks of March
 
-$ flights-cli AMS ATH --depart-from 01.03 --depart-to 15.03
+$ airflair AMS ATH --depart-from 01.03 --depart-to 15.03
 
 Round-trips from Amsterdam to Athens on specific dates
 
-$ flights-cli AMS ATH --depart 01.03 --return 12.03
+$ airflair AMS ATH --depart 01.03 --return 12.03
 
 Round-trips with a specific departure date, but a variety of return dates
 
-$ flights-cli AMS ATH --depart 01.03 --return-from 05.03 --return-to 08.03
+$ airflair AMS ATH --depart 01.03 --return-from 05.03 --return-to 08.03
 
 Round-trips spanning a specified number of days
 
-$ flights-cli AMS ATH --depart-from 01.03 --depart-to 03.03 --days 5
-$ flights-cli AMS ATH --depart 01.03 --days-from 5 --days-to 7
+$ airflair AMS ATH --depart-from 01.03 --depart-to 03.03 --days 5
+$ airflair AMS ATH --depart 01.03 --days-from 5 --days-to 7
 
 Multi-leg trip: travel from Amsterdam to Athens on the first weekend in March, stay for 5 days, then travel to Warsaw, and on the 12th return to Amsterdam
 
-$ flights-cli AMS --depart-from 01.03 --depart-to 03.03 \\
+$ airflair AMS --depart-from 01.03 --depart-to 03.03 \\
               ATH --stay 5 \\
               WAW --until 12.03 \\
               AMS
